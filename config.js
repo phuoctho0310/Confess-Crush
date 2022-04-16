1ca9aaed9a985c39e4ebd46822ef2ad9989db0db
PhuocTho <88882008+phuoctho0310@users.noreply.github.com>
2022-04-16 16:39:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "My Love",
     introTitle: 'Meow Meow',
-    introDesc: `Vào một ngày nắng hạ.
-     Trong tim anh có em.
-     Đôi lúc lại bâng khuâng.
-     Sao iu em đến thế ?`,
+    introDesc: `Mãi ngắm hoàng hôn em quên luôn trời tối.
+    Mãi ngắm chị cười em quên luôn cả thanh xuân.`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Làm người iu anh nhá <33',
-    btnNo: 'Không iu đâu :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnYes: 'Làm người iu em nhá <33',
+    btnNo: 'Hăm được đou :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay rãnh anh qua đón đi chơi nha.',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘. Meow Meow',
+    messDesc: 'Em bé xinh đẹp tuần sau đi chơi nhá',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/100032008661434' //link mess của các bạn. VD: https://m.me/100032008661434
 }
